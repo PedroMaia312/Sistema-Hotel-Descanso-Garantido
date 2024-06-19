e71145cdd30f6fbdd818c3c3642da56df9f0e564
unknown <pedromaiasilva17@gmail.com>
2024-06-19 14:48:33 -0300

    Melhora das funções de Cadastro de quarto e estadia

--- Code/main.c
+++ Code/main.c
@@ -44,6 +44,7 @@ void cadastroQuarto ();
 void achaEstadia ();
 void pesquisaPessoas ();
 void finalizarEstadia ();
+int calculaDiarias ();
 int main()
 {
     setlocale(LC_ALL, "portuguese");
@@ -105,75 +106,102 @@ void cadastraEstadia ()
         printf("Ocorreu um error ao abrir um arquivo");
         fclose(estadia);
     }
-    struct Estadia registro;
-    registro.codigoEstadia = cE;
-
     do
     {
-        printf("Digite o código do Cliente que deseja se hospedar:\n");
-        scanf("%d", &registro.codigoCliente);
+        struct Estadia registro;
+        registro.codigoEstadia = cE;
+        fprintf(estadia,"Estadia: %d {\n", registro.codigoEstadia);
+        do
+        {
+            printf("Digite o código do Cliente que deseja se hospedar:\n");
+            scanf("%d", &registro.codigoCliente);
+        }while(registro.codigoCliente < 1);
+
+        do
+        {
+            printf("Digite o dia de entrada do cliente:\n");
+            scanf("%d", &registro.entrada[1]);
+        }while(registro.entrada[1] < 1 || registro.entrada[1] > 31);
+        do
+        {
+            printf("Digite o mês de entrada do cliente:\n");
+            scanf("%d", &registro.entrada[2]);
+        }while(registro.entrada[2] < 1 || registro.entrada[1] > 12);
+        do
+        {
+            printf("Digite o ano de entrada do cliente:\n");
+            scanf("%d", &registro.entrada[3]);
+        }while(registro.entrada[3] < 2024);
+        fprintf(estadia, "Data da entrada: %d/%d/%d", registro.entrada[1],registro.entrada[2],registro.entrada[3]);
+        do
+        {
+            printf("Digite o dia de saída do cliente:\n");
+            scanf("%d", &registro.saida[1]);
+        }while(registro.saida[1] < 1 || registro.saida[1] > 31);
+        do
+        {
+            printf("Digite o mês de saída do cliente:\n");
+            scanf("%d", &registro.saida[2]);
+        }while(registro.saida[2] < 1 || registro.saida[2] > 12);
+        do
+        {
+            printf("Digite o ano de saída do cliente:\n");
+            scanf("%d", &registro.saida[3]);
+        }while(registro.saida[3] < registro.entrada[3]);
+        fprintf(estadia, "Data da saída: %d/%d/%d", registro.saida[1],registro.saida[2],registro.saida[3]);
+
+
+        int dias = calculaDiarias (registro.entrada[3], registro.saida[3]);
+
+
+        printf("Deseja cadastrar mais alguma estadia('s' para sim e 'n' para não):\n");
+        scanf(" %c", &estadia2);
     }
-    while(registro.codigoCliente < 1);
-
-    do{
-    printf("Digite o dia de entrada do cliente:\n");
-    scanf("%d", &registro.entrada[1]);
-    }while(registro.entrada[1] < 1 || registro.entrada[1] > 31);
-
-    do{
-    printf("Digite o mês de entrada do cliente:\n");
-    scanf("%d", &registro.entrada[2]);
-    }while(registro.entrada[2] < 1 || registro.entrada[1] > 12);
-
-    do{
-    printf("Digite o ano de entrada do cliente:\n");
-    scanf("%d", &registro.entrada[3]);
-    }while(registro.entrada[3] < 2024);
-
-    do{
-    printf("Digite o dia de saída do cliente:\n");
-    scanf("%d", &registro.saida[1]);
-    }while(registro.saida[1] < 1 || registro.saida[1] > 31);
-
-    do{
-    printf("Digite o mês de saída do cliente:\n");
-    scanf("%d", &registro.saida[2]);
-    }while(registro.saida[2] < 1 || registro.saida[2] > 12);
-
-    do{
-    printf("Digite o ano de saída do cliente:\n");
-    scanf("%d", &registro.saida[3]);
-    }while(registro.saida[3] < registro.entrada[3]);
-
+    while(estadia2 != 'n');
     fclose(estadia);
-
+    cE++;
 }
 void cadastroQuarto ()
 {
     char quarto2;
     FILE *quarto;
     quarto = fopen("C:\\Github\\Sistema-Hotel-Descanso-Garantido\\Code\\Txt\\Quartos.txt", "r+");
+    struct Quarto registro;
+    if(!quarto)
+    {
+        printf("Ocorreu um error ao abrir um arquivo");
+        exit(1);
+    }
     do
     {
-        if(!quarto)
+        do
         {
-            printf("Ocorreu um error ao abrir um arquivo");
-            exit(1);
+            printf("Qual o numero do quarto que você deseja registrar:\n");
+            scanf("%d", &registro.numeroQuarto);
         }
-        struct Quarto registro;
-        printf("Qual o numero do quarto que você deseja registrar:\n");
-        scanf("%d", &registro.numeroQuarto);
-        printf("Quantos hospestes cabem dentro deste quarto:\n");
-        scanf("%d", &registro.hospedes);
-        printf("Quanto é a diaria deste quarto:\n");
-        scanf("%f", &registro.valorDiario);
+        while(registro.numeroQuarto < 100);
+        fprintf(quarto, "Quarto %d \n{\n", registro.numeroQuarto);
+        do
+        {
+            printf("Quantos hospestes cabem dentro deste quarto:\n");
+            scanf("%d", &registro.hospedes);
+        }
+        while(registro.hospedes < 0);
+        fprintf(quarto, "Quantos hospedes cabem: %d \n", registro.hospedes);
+        do
+        {
+            printf("Quanto é a diaria deste quarto:\n");
+            scanf("%f", &registro.valorDiario);
+        }
+        while(registro.valorDiario < 0.01);
+        fprintf(quarto,"Valor da diaria: %.2f \n", registro.valorDiario);
         do
         {
             printf("Qual o status do quarto ele esta desocupado ou ocupado:\n");
             gets(registro.status);
         }
         while((strcmp(registro.status, "desocupado") != 0 && strcmp(registro.status, "ocupado") != 0));
-        fprintf(quarto, "%d, %d, %.2f, %s\n", registro.numeroQuarto, registro.hospedes, registro.valorDiario, registro.status);
+        fprintf(quarto, "Status do quarto: %s \n}\n", registro.status);
         printf("Você deseja cadastrar mais um quarto('s' para sim e 'n' para não):\n");
         scanf(" %c", &quarto2);
     }
@@ -193,4 +221,32 @@ void pesquisaPessoas ()
 void achaEstadia ()
 {
 
+}
+int calculaDiarias (int entrada[3], int saida[3])
+{
+    int dias = 0;
+    if(entrada[2] < saida[2])
+    {
+        dias = 31 * (saida[2] - entrada[2]);
+    }
+    if(entrada[3] < saida[3])
+    {
+        if(dias == 0)
+        {
+            dias = 365 * (saida[3] - entrada[3]);
+        }
+        else
+        {
+            dias = dias + 365 * (saida[3] - entrada[3]);
+        }
+    }
+    if(dias == 0)
+    {
+        dias = saida[1] - entrada [1];
+    }
+    else
+    {
+        dias = dias + (saida[1] - entrada [1]);
+    }
+    return dias;
 }
