8f162207e8d3ea7d6599f87665a7988c142c921a
unknown <joaovslopes20@gmail.com>
2024-06-21 12:44:42 -0300

    adição das funções para cadastrar funcionarios e clientes, sem o menu

--- Code/main.c
+++ Code/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
+#include <ctype.h>
 
 struct Cliente
 {
@@ -15,7 +17,7 @@ struct Funcionarios
     char nome[100];
     char numero[11];
     char cargo[50];
-    float salario;
+    char salario[50];
 };
 struct Estadia
 {
@@ -43,58 +45,112 @@ void finalizarEstadia ();
 int main()
 {
     setlocale(LC_ALL, "portuguese");
-    int menu;
-    do
-    {
-        printf("Escolha oque voce ira fazer no sistema:\n");
-        printf(" 1 - Cadastras cliente \n 2 - Cadastrar funcionario \n 3 - Cadastrar Estadia\n 4 - Pesquisar por pessoas \n 5 - Baixa em Estadia \n 0 - Sair \n");
-        scanf("%d", &menu);
-        if(menu < 0 || menu > 5)
-        {
-            printf("Digite entre as opções do menu! \n");
-        }
-        switch(menu)
-        {
-        case 0:
-                printf("Saindo do Programa...");
-            break;
-        case 1:
-            cadastraCliente();
-            break;
-        case 2:
-            cadastraFuncionario();
-            break;
-        case 3:
-            cadastraEstadia();
-            break;
-        case 4:
-            pesquisaPessoas();
-            break;
-        case 5:
-            finalizarEstadia();
-            break;
-
-        }
-    }
-    while(menu != 0);
+    cadastraCliente();
+    cadastraFuncionario();
     return 0;
 }
 
-void cadastraCliente ()
+void cadastraCliente()
 {
-    printf("entrou");
-
+    FILE *fluxo;
+    fluxo = fopen("Clientes.txt", "a+");
+    struct Cliente clienteatual;
+    int erro = 0;
+    do{
+    erro = 0;
+    printf("digite o nome do cliente:\n");
+    gets(clienteatual.nome);
+    for (int i = 0;i < strlen(clienteatual.nome);i++)
+    {
+        if(isalpha(clienteatual.nome[i]) == 0 && clienteatual.nome[i] != ' ')
+        {
+            erro++;
+        }
+    }
+    printf("digite o endereço do cliente:\n");
+    gets(clienteatual.endereco);
+    printf("digite o numero do cliente:\n");
+    gets(clienteatual.numero);
+    for (int i = 0;i < strlen(clienteatual.numero);i++)
+    {
+        if(isdigit(clienteatual.numero[i]) == 0)
+        {
+            erro++;
+        }
+    }
+    }while(erro != 0);
+    char linha[200];
+    char ultimalinha[200];
+    char delimitador[] = ";";
+    while(fgets(linha, sizeof(linha), fluxo) != NULL)
+    {
+        strcpy(ultimalinha, linha);
+    }
+    clienteatual.codigo = atoi(strtok(ultimalinha, delimitador));
+    fprintf(fluxo, "\n%d;%s;%s;%s;", clienteatual.codigo + 1,clienteatual.nome,clienteatual.endereco,clienteatual.numero);
+    fclose(fluxo);
 }
 
-void cadastraFuncionario ()
+void cadastraFuncionario()
 {
-
-     printf("entrou");
+    FILE *fluxo;
+    fluxo = fopen("Funcionarios.txt", "a+");
+    struct Funcionarios funcionarioatual;
+    int erro = 0;
+    do{
+    erro = 0;
+    printf("digite o nome do funcionario:\n");
+    gets(funcionarioatual.nome);
+    for (int i = 0;i < strlen(funcionarioatual.nome);i++)
+    {
+        if(isalpha(funcionarioatual.nome[i]) == 0 && funcionarioatual.nome[i] != ' ')
+        {
+            erro++;
+        }
+    }
+    printf("digite o cargo do funcionario:\n");
+    gets(funcionarioatual.cargo);
+    for (int i = 0;i < strlen(funcionarioatual.cargo);i++)
+    {
+        if(isalpha(funcionarioatual.cargo[i]) == 0 && funcionarioatual.cargo[i] != ' ')
+        {
+            erro++;
+        }
+    }
+    printf("digite o numero do funcionario:\n");
+    gets(funcionarioatual.numero);
+    for (int i = 0;i < strlen(funcionarioatual.numero);i++)
+    {
+        if(isdigit(funcionarioatual.numero[i]) == 0)
+        {
+            erro++;
+        }
+    }
+    printf("digite o salario do funcionario:\n");
+    gets(funcionarioatual.salario);
+    for (int i = 0;i < strlen(funcionarioatual.salario);i++)
+    {
+        if(isdigit(funcionarioatual.salario[i]) == 0 && funcionarioatual.salario[i] != ',')
+        {
+            erro++;
+        }
+    }
+    }while(erro != 0);
+    char linha[200];
+    char ultimalinha[200];
+    char delimitador[] = ";";
+    while(fgets(linha, sizeof(linha), fluxo) != NULL)
+    {
+        strcpy(ultimalinha, linha);
+    }
+    funcionarioatual.codigo = atoi(strtok(ultimalinha, delimitador));
+    fprintf(fluxo, "\n%d;%s;%s;%s;%s;", funcionarioatual.codigo + 1,funcionarioatual.nome,funcionarioatual.cargo,funcionarioatual.numero,funcionarioatual.salario);
+    fclose(fluxo);
 }
 void cadastraEstadia ()
 {
     printf("teste git");
-     printf("entrou");
+    printf("entrou");
 }
 void cadastroQuarto ()
 {
